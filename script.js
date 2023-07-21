f7dde52ffad4c5751431322547329d8206a0ba26
Umang <umanggg100@gmail.com>
2023-07-21 10:08:24 +0530

    add popup window for read more

--- script.js
+++ script.js
@@ -9,6 +9,18 @@ menuIcon.onclick = () => {
 
 // |||||||||||||||| POPUP |||||||||||||||||||||
 
+myButton.addEventListener("click", function () {
+    myPopup.classList.add("show");
+});
+closePopup.addEventListener("click", function () {
+    myPopup.classList.remove("show");
+});
+window.addEventListener("click", function (event) {
+    if (event.target == myPopup) {
+        myPopup.classList.remove("show");
+    }
+});
+
 
 
 // |||||||||||| SCROLL SECTION ACTIVE LINK |||||||||||
